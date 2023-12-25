95e464646afb1e18b7887ea2edafcd0ac0f9c1a6
Igor Moskalev <moskalev.igor.v@gmail.com>
2023-12-26 04:50:49 +0600

    feat: implement queue, stack, remove from list tasks

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,8 +1,8 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require("../extensions/list-node.js");
 
-/**
+/*
  * Given a singly linked list of integers l and an integer k,
  * remove all elements from list l that have a value equal to k.
  *
@@ -22,9 +22,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (!l) {
+    return null;
+  }
+
+  const node = new ListNode(0);
+  node.next = l;
+
+  let current = node;
+
+  while (current.next) {
+    if (current.next.value === k) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+  return node.next;
 }
 
 module.exports = {
